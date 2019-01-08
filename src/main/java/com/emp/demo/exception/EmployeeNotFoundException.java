e610042371a1695a145f62e2d64d158852b8e18f
Sanjay <kanthetisanjay7@gmail.com>
2019-01-08 10:51:26 +0530

    First Code Commit

---
+++ src/main/java/com/emp/demo/exception/EmployeeNotFoundException.java
@@ -0,0 +1,10 @@
+package com.emp.demo.exception;
+
+public class EmployeeNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public EmployeeNotFoundException(long id){
+	    super("EmployeeId " + id + " does not exist.");
+	  }
+}
